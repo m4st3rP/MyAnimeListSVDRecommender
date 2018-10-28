284c17677e141694d1faf4c87d9145edfb1288ef
Philipp Schwarz <schwarz_philipp@hotmail.de>
2018-10-29 00:41:06 +0100

    added some type annotations

--- scores_updater/UserDataDownloader.py
+++ scores_updater/UserDataDownloader.py
@@ -9,8 +9,8 @@ class UserDataDownloader:
         print()  # TODO Remove
 
     def get_user_anime_list_and_save_to_db(self, user_name):
-        last_page = False
-        offset = 0
+        last_page: bool = False
+        offset: int = 0
 
         while not last_page:
             animes_list_page = requests.get("https://myanimelist.net/animelist/{}/load.json?offset={}&status=7".format(user_name, offset)).json()
@@ -20,7 +20,7 @@ class UserDataDownloader:
 
             for anime_json_entry in animes_list_page:
                 if anime_json_entry["score"] > 0:  # only add scores that were actually set (0 means none)
-                    anime_db_entry = Anime(id=anime_json_entry["anime_id"], name=anime_json_entry["anime_title"])  # TODO add check if anime is already in list
+                    anime_db_entry = Anime(id=anime_json_entry["anime_id"], name=anime_json_entry["anime_title"])  # TODO add check if anime is already in db
                     anime_db_entry.save()
 
                     anime_score_db_entry = AnimeScore(anime_id=anime_json_entry["anime_id"], user_name=user_name, score=anime_json_entry["score"])
